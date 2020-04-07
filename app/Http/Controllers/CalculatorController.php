6a44883f6279dbe6d1ede06c0ac39cb6453fb606
Henry-Harvey <46501451+Henry-Harvey@users.noreply.github.com>
2020-04-07 12:04:35 -0500

    comments

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -14,6 +14,7 @@ class CalculatorController extends Controller
      * Controller method that takes in a request
      * Sets the data from the request to variables
      * Creates a Calculation object from the variables
+     * Creates a calculation bs and calls its newCalculation method with the calculation
      * Creates an associative array with the object
      * Returns result view and pushes the data array
      *
@@ -25,39 +26,63 @@ class CalculatorController extends Controller
     {
         MyLogger::info("\Entering " . substr(strrchr(__METHOD__, "\\"), 1));
 
+        // Sets the data from the request to variables
         $title = $request->input('title');
         $years = $request->input('years');
         $months = $request->input('months');
         $days = $request->input('days');
         $hours = $request->input('hours');
 
+        // Creates a Calculation object from the variables
         $c = new Calculation($title, $years, $months, $days, $hours);
+        
+        // Creates a calculation bs and calls its newCalculation method with the calculation
         $bs = new CalculationBusinessService();
         $bs->newCalculation($c);
 
+        // Creates an associative array with the object
         $data = [
             'c' => $c
         ];
+        
+        // Returns result view and pushes the data array
         MyLogger::info("/Exiting  " . substr(strrchr(__METHOD__, "\\"), 1) . " to result view with result");
         return view('result')->with($data);
     }
 
+    /**
+     * Creates a calculation bs and calls its getAllCalculations method
+     * If the result is null, returns the calculator view
+     * Else creates an associative array with the object
+     * Returns allCalculations view and pushes the data array
+     *
+     * @param Request $request
+     *            Implicit request
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory allCalculations view
+     */
     public function onGetAll()
     {
         MyLogger::info("\Entering " . substr(strrchr(__METHOD__, "\\"), 1));
         try {
+            
+            // Creates a calculation bs and calls its getAllCalculations method
             $bs = new CalculationBusinessService();
             $flag = $bs->getAllCalculations();
 
+            
+            // If the result is null, returns the calculator view
             if ($flag == null) {
                 MyLogger::warning("flag returned null");
                 MyLogger::info("/Exiting  " . substr(strrchr(__METHOD__, "\\"), 1) . " to calculator view");
                 return view('calculator');
             }
 
+            // Else creates an associative array with the object
             $data = [
                 'allCalculations' => $flag
             ];
+            
+            // Returns allCalculations view and pushes the data array
             MyLogger::info("/Exiting  " . substr(strrchr(__METHOD__, "\\"), 1) . " to allCalculations view with all calculations");
             return view('allCalculations')->with($data);
         } catch (Exception $e) {
--- app/Models/Services/Business/CalculationBusinessService.php
+++ app/Models/Services/Business/CalculationBusinessService.php
@@ -8,32 +8,61 @@ use App\Models\Services\Data\CalculationDataService;
 class CalculationBusinessService
 {
 
+    /**
+     * Takes in a caluclation model to be added
+     * Creates a new database model and gets the database from it
+     * Creates calclation data service and calls create method with the new calculation
+     * Sets db to null
+     * Returns flag
+     *
+     * @param
+     *            newCalculation calculation to be created
+     * @return {@link Integer} number of rows affected
+     */
     function newCalculation($newCalculation)
     {
         MyLogger::info("\Entering " . substr(strrchr(__METHOD__, "\\"), 1) . " with " . $newCalculation);
 
+        // Creates a new database model and gets the database from it
         $Database = new DatabaseModel();
         $db = $Database->getDb();
 
+        // Creates calclation data service and calls create method with the new calculation
         $ds = new CalculationDataService($db);
-
         $flag = $ds->create($newCalculation);
+        
+        // Sets db to null
+        $db = null;
 
+        // Returns flag
         MyLogger::info("/Exiting  " . substr(strrchr(__METHOD__, "\\"), 1) . " with " . $flag);
         return $flag;
     }
 
+    /**
+     * Creates a new database model and gets the database from it
+     * Creates calculation data service and calls readAll method
+     * Sets db to null
+     * Returns flag
+     *
+     * @return    Array array of calculations found
+     */
     function getAllCalculations()
     {
         MyLogger::info("\Entering " . substr(strrchr(__METHOD__, "\\"), 1));
 
+        // Creates a new database model and gets the database from it
         $Database = new DatabaseModel();
         $db = $Database->getDb();
 
+        // Creates calculation data service and calls readAll method
         $ds = new CalculationDataService($db);
-
         $flag = $ds->readAll();
+        
+        // Sets db to null
+        $db = null;
 
+        // Returns flag
         MyLogger::info("/Exiting  " . substr(strrchr(__METHOD__, "\\"), 1) . " with array of calculations");
         return $flag;
     }
